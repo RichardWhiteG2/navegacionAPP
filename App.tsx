daf6962cb4a462f756748ec318fafb085f5146ba
Brayan Ricardo Blanco García <102545085+RichardWhiteG2@users.noreply.github.com>
2023-03-04 08:40:10 -0600

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hola Mundo</Text>
+    </View>
+  )
+}
+export default App;
