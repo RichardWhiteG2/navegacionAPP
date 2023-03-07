a43e855ba5e38e2196c0ab9fe2e955c1bacc3d90
Brayan Ricardo Blanco García <102545085+RichardWhiteG2@users.noreply.github.com>
2023-03-07 00:24:15 -0600

    Mostrar/Ocultar Drawer

--- src/navigator/MenuLateralBasico.tsx
+++ src/navigator/MenuLateralBasico.tsx
@@ -3,15 +3,21 @@ import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { SettingsScreen } from '../screens/SettingsScreen';
 import { StackNavigator } from './NavigatorStack';
+import { useWindowDimensions } from 'react-native';
+
 
 const Drawer = createDrawerNavigator();
 
 export const MenuLateralBasico = () => {
 
+    const { width } = useWindowDimensions();
+
   return (
-    <Drawer.Navigator>
-      <Drawer.Screen name="StackNavigator" component={StackNavigator} />
-      <Drawer.Screen name="SettingsScreen" component={SettingsScreen} />
+    <Drawer.Navigator
+        screenOptions={{ drawerType: width >= 768 ? 'permanent' : 'front',}}
+    >
+      <Drawer.Screen name="StackNavigator" options={{title:"Home"}}component={StackNavigator} />
+      <Drawer.Screen name="SettingsScreen" options={{title:"Settings"}}component={SettingsScreen} />
     </Drawer.Navigator>
   );
 }
--- src/screens/Pagina1Screen.tsx
+++ src/screens/Pagina1Screen.tsx
@@ -1,11 +1,27 @@
+import { DrawerScreenProps } from '@react-navigation/drawer'
 import { StackScreenProps } from '@react-navigation/stack'
-import React from 'react'
-import { Button, Text, View } from 'react-native'
-import { TouchableOpacity } from 'react-native-gesture-handler'
+// import { StackScreenProps } from '@react-navigation/stack'
+import React, { useEffect } from 'react'
+import { Button, Text, View, TouchableOpacity } from 'react-native'
 import { styles } from '../theme/appTheme'
 
 interface Props extends StackScreenProps<any, any>{};
+// interface Props extends DrawerScreenProps<any, any>{};
 export const Pagina1Screen = ({navigation}: Props) => {
+
+  // useEffect(() => {
+    
+  //   navigation.setOptions({
+  //     headerLeft: ()=>{
+  //       return <Button
+  //         title='Menu'
+  //         onPress={() => navigation.toggleDrawer()} />
+  //     }
+  //   })
+  // }, [])
+  
+
+
   return (
     <View style={ styles.globalMargin}>
         <Text style ={ styles.title}> Pagina1Screen</Text>  
