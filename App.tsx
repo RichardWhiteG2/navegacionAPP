5708c906394bfe4c0d0a5210988d80d40a2e877c
Brayan Ricardo Blanco García <102545085+RichardWhiteG2@users.noreply.github.com>
2023-03-06 20:00:24 -0600

    Se prepara para instalar react-native-reanimated

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native'
 import React from 'react'
-import { StackNavigator } from './src/navigator/NavigatorStack';
+// import { StackNavigator } from './src/navigator/NavigatorStack';
 import { View } from 'react-native';
+import { MenuLateralBasico } from './src/navigator/MenuLateralBasico';
+import { StackNavigator } from './src/navigator/NavigatorStack';
 
 const App = () => {
   return (
     <NavigationContainer>
+      
+        {/* <MenuLateralBasico/> */}
+
       
         <StackNavigator/>
       
