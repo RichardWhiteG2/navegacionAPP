5042ac72ba0b499deebd6836dbe952bf183e5926
Brayan Ricardo Blanco García <102545085+RichardWhiteG2@users.noreply.github.com>
2023-03-07 20:57:13 -0600

    Preparando el espacio para el Icono

--- src/navigator/Tabs.tsx
+++ src/navigator/Tabs.tsx
@@ -4,6 +4,7 @@ import { Tab1Screen } from '../screens/Tab1Screen';
 import { Tab2Screen } from '../screens/Tab2Screen';
 import { StackNavigator } from './NavigatorStack';
 import { Colores } from '../theme/appTheme';
+import { Text } from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
@@ -14,12 +15,13 @@ export const Tabs = () => {
         
         tabBarActiveTintColor: Colores.primary,
         tabBarStyle: { borderTopColor: Colores.primary , borderTopWidth:10, elevation:0},
-        
+        tabBarLabelStyle: {fontSize:20}
     }}
     sceneContainerStyle={{backgroundColor:"white"}}
+    
 
     >
-      <Tab.Screen name="Tab1Screen" options={{title:'Tab1'}}component={Tab1Screen} />
+      <Tab.Screen name="Tab1Screen" options={{title:'Tab1', tabBarIcon:(props)=> <Text>T1</Text>}}component={Tab1Screen} />
       <Tab.Screen name="Tab2Screen" options={{title:'Tab2'}}component={Tab2Screen} />
       <Tab.Screen name="StackNavigator" options={{title:'Stack'}}component={StackNavigator} />
       
