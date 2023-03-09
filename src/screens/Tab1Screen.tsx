168a145bd61def0724d1b5594ada4f24981a0df0
Brayan Ricardo Blanco García <102545085+RichardWhiteG2@users.noreply.github.com>
2023-03-09 00:53:05 -0600

    Instalacion de ICONS A/I

--- src/screens/Tab1Screen.tsx
+++ src/screens/Tab1Screen.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import { styles } from '../theme/appTheme'
+
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export const Tab1Screen = () => {
   return (
-    <View>
-        <Text>Tab 1 Screen</Text>
+    <View style={styles.globalMargin}>
+        <Text style={styles.title}>Icons</Text>
+        <Text><Icon name="airplane-outline" size={200} color="#900" /></Text>
     </View>
   )
 }
