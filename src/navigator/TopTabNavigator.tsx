f6d804abc1ee0fa20e4b402d39c28c3bba88da67
Brayan Ricardo Blanco García <102545085+RichardWhiteG2@users.noreply.github.com>
2023-03-08 20:35:14 -0600

    TopTabNavigator STYLE

--- src/navigator/TopTabNavigator.tsx
+++ src/navigator/TopTabNavigator.tsx
@@ -1,15 +1,38 @@
 
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import React from 'react';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 import { AlbumsScreen } from '../screens/AlbumsScreen';
 import { ChatScreen } from '../screens/ChatScreen';
 import { ContactsScreen } from '../screens/ContactsScreen';
+import { Colores } from '../theme/appTheme';
 
 const Tab = createMaterialTopTabNavigator();
 
 export const TopTabNavigator = () => {
+
+
+
+    const {top } =useSafeAreaInsets()
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+        style={{
+            // paddingTop: top
+        }}
+        sceneContainerStyle={{
+            backgroundColor: 'white'
+        }}
+        screenOptions={{
+            tabBarStyle:{
+                shadowColor:'transparent',
+                elevation:0
+            },
+            tabBarPressColor: Colores.primary,
+            tabBarIndicatorStyle:{ backgroundColor: Colores.primary}
+        }}
+        
+    >
       <Tab.Screen name="ChatScreen" component={ChatScreen} />
       <Tab.Screen name="ContactsScreen" component={ContactsScreen} />
       <Tab.Screen name="AlbumsScreen" component={AlbumsScreen} />
